754e749c396e88521107eff0cc0a7cc0ada2ec01
Aldo Díaz Betizagasti <aldo@kzsoftworks.com>
2023-04-28 17:47:27 -0300

    Add autologin feature
    Including context menu to disable in case you want to login with a different user

--- background.js
+++ background.js
@@ -1,5 +1,43 @@
-chrome.action.onClicked.addListener(function(tab) {
-	if (tab){
-		chrome.tabs.sendMessage(tab.id, {action: 'copy-token'});
-	}
+chrome.action.onClicked.addListener(function (tab) {
+    if (tab) {
+        chrome.tabs.sendMessage(tab.id, {
+            action: 'copy-token'
+        });
+    }
 });
+
+chrome.tabs.onUpdated.addListener(
+    (a, b, c) => {
+        if (autoLogin && b.status == "complete" && c.url.indexOf("login") != -1 &&
+            (c.url.indexOf("smartborder") != -1 || c.url.indexOf("localhost") != -1))
+            chrome.tabs.sendMessage(a, {
+                action: 'auto-login'
+            });
+
+    });
+
+chrome.contextMenus.create({
+    id: 'autoLogin',
+    title: 'Disable auto login',
+    contexts: ['all'],
+})
+
+var autoLogin = true;
+
+chrome.contextMenus.onClicked.addListener(
+    (data) => {
+        if (data.menuItemId == "autoLogin") {
+            var properties = autoLogin ? {
+                title: 'Enable auto login'
+            } : {
+                title: 'Disable auto login'
+            };
+            chrome.contextMenus.update(
+                'autoLogin',
+                properties
+            )
+
+            autoLogin = !autoLogin;
+        }
+    }
+)
--- content.js
+++ content.js
@@ -1,49 +1,70 @@
 function customConsole() {
-	function getToken() {
-		try {
-		   var el = document.createElement('textarea');
-		   el.value = JSON.parse(localStorage.userSessions)[0].Token;
-		   el.setAttribute('readonly', '');
-		   document.body.appendChild(el);
-		   el.select();
-		   document.execCommand('copy');
-		   document.body.removeChild(el);
-		   console.log('Token copiado');
-		} catch (error){
-			console.log('El token no existe');
-		}
-	}
-	window.gt = getToken;
+    function getToken() {
+        try {
+            var el = document.createElement('textarea');
+            el.value = JSON.parse(localStorage.userSessions)[0].Token;
+            el.setAttribute('readonly', '');
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+            console.log('Token copiado');
+        } catch (error) {
+            console.log('El token no existe');
+        }
+    }
+    window.gt = getToken;
 }
 
 function getToken() {
-	try {
-	   var el = document.createElement('textarea');
-	   el.value = JSON.parse(localStorage.userSessions)[0].Token;
-	   el.setAttribute('readonly', '');
-	   document.body.appendChild(el);
-	   el.select();
-	   document.execCommand('copy');
-	   document.body.removeChild(el);
-	   console.log('Token copiado');
-	} catch (error){
-		console.log('El token no existe');
-	}
+    try {
+        var el = document.createElement('textarea');
+        el.value = JSON.parse(localStorage.userSessions)[0].Token;
+        el.setAttribute('readonly', '');
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        console.log('Token copiado');
+    } catch (error) {
+        console.log('El token no existe');
+    }
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
-	// handle icon click
-	if(!!request){
-		switch(request.action){
-			case 'copy-token':
-				this.getToken();
-				break;
-		}
-	}
-    
+function autoLogin() {
+    try {
+        setTimeout(() => {
+            var usernameInput = document.getElementById("username");
+            usernameInput.value = "test2@kzsoftworks.com";
+            usernameInput.dispatchEvent(new Event('input', {bubbles: true}));
+
+            var pwInput = document.getElementById("password");
+            pwInput.value = "1234";
+            pwInput.dispatchEvent(new Event('input', {bubbles: true}));
+
+            document.getElementById("loginButton").click();
+        }, 10);
+    } catch (error) {
+        console.log('El token no existe');
+    }
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+    // handle icon click
+    if (!!request) {
+        switch (request.action) {
+            case 'copy-token':
+                this.getToken();
+                break;
+            case 'auto-login':
+                this.autoLogin();
+                break;
+        }
+    }
+
 });
 
 var script = document.createElement('script'),
-    code   = document.createTextNode('(' + customConsole + ')();');
+    code = document.createTextNode('(' + customConsole + ')();');
 script.appendChild(code);
 (document.body || document.head || document.documentElement).appendChild(script);
